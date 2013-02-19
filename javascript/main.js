04b5819b9257aab58a868eec92529cc66bcf63a9
mkindt <mkindt@unca.edu>
2013-02-19 12:31:54 -0500

    Improved readability with fade instead of scroll, modified overall look

--- javascript/main.js
+++ javascript/main.js
@@ -1,37 +1,35 @@
 
 var main = function () {
  var count = 0;
+ var rows = 0;
  var twitter = new ctwitter.CTwitter();
- twitter.stream("statuses/filter", { track:["best asheville"] }, function (stream){
+ twitter.stream("statuses/filter", { track:["segway"] }, function (stream){
   stream.on("data", function(tweet) {
    console.log(tweet.text);
     if (count < 10){
-     $(".content").append("<p class='tweet'>"+tweet.text+"</p>");
+     $(".content").append("<p class='tweet' style='display:none'>"+tweet.text+"</p>");
+     $(".tweet:eq("+count+")").fadeIn(1200);
      count = count + 1;
-     //$(".content").remove("<p class 'tweet'>"+tweet.text+"</p>");
-     //$(".content").append("<img src='"+tweet.profile_image_url+"' />" );
     }
     else {
-      $(".tweet:first").slideUp(500, function() {
-        $(".tweet:first").remove();
-  });
-      $(".content").append("<p class='tweet' style='display:none'>"+tweet.text+"</p>");
-      $(".tweet:last").slideDown(500);
+     if (rows===10){
+      rows = 0;
+     }
+      $(".tweet:eq("+rows+")").fadeOut(600, function() {
+        $(".tweet:eq("+rows+")").replaceWith("<p class='tweet' style='display:none'>"+tweet.text+"</p>");
+        $(".tweet:eq("+rows+")").fadeIn(600);
+        rows = rows + 1;
+      });
     }
+
   });
  });
  console.log(ctwitter);
  console.log("hello!!!!");
- //console.log($);
- $(".content").append("<p>Hello from jQuery...</p>");
- //$(".content").html("<p>Hello from jQuery2...</p>");
- $(".content").append("<p>Hello from jQuery3...</p>");
- $(".content").append("<p id='watch1' style='display:none'>What about this?</p>");
- $("#watch1").slideDown(500, function() {
-  //alert("slide down is done");
- });
 
+ $(".content").append("<p id='watch1' style='display:none'>Twitter analysis initiated:</p>");
+ $("#watch1").slideDown(1500);
 }
 
 $(document).ready(main);
-//main();
+
