c65a7e07b55e53044f084195207289c627c081f9
mkindt <mkindt@unca.edu>
2013-02-18 22:51:12 -0500

    initial commit of javascript code and barebones html/css

---
+++ javascript/main.js
@@ -0,0 +1,34 @@
+
+var main = function () {
+ var count = 0;
+ var twitter = new ctwitter.CTwitter();
+ twitter.stream("statuses/filter", { track:["best asheville"] }, function (stream){
+  stream.on("data", function(tweet) {
+   console.log(tweet.text);
+    if (count < 10){
+     $(".content").append("<p class 'tweet'>"+tweet.text+"</p>");
+     count = count + 1;
+     //$(".content").remove("<p class 'tweet'>"+tweet.text+"</p>");
+     //$(".content").append("<img src='"+tweet.profile_image_url+"' />" );
+    }
+    else {
+     $("p:first").remove();
+     $(".content").append("<p class 'tweet'>"+tweet.text+"</p>");;
+    }
+  });
+ });
+ console.log(ctwitter);
+ console.log("hello!!!!");
+ //console.log($);
+ $(".content").append("<p>Hello from jQuery...</p>");
+ //$(".content").html("<p>Hello from jQuery2...</p>");
+ $(".content").append("<p>Hello from jQuery3...</p>");
+ $(".content").append("<p id='watch1' style='display:none'>What about this?</p>");
+ $("#watch1").slideDown(500, function() {
+  //alert("slide down is done");
+ });
+
+}
+
+$(document).ready(main);
+//main();
